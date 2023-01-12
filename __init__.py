2e7199352a01979929043e10082f2dd28981e408
EMM <3209970865@qq.com>
2023-01-12 11:26:50 +0800

    change .gitignore

--- __init__.py
+++ __init__.py
@@ -1,14 +1,13 @@
-bl_info = {
 
-    "name": "Gesture Helper",
-    "description": "手势助手,可以快速的使用手势运行blender 操作符或是更改属性",
-    "author": "AIGODLIKE Community(BlenderCN辣椒,小萌新)",
-    "version": (1, 0),
-    "blender": (3, 0, 0),
-    # "location": "Tool Panel",
-    "support": "COMMUNITY",
-    # "category": "3D View",
-}
+bl_info = {"name": "Gesture Helper",
+           "description": "手势助手,可以快速的使用手势运行blender 操作符或是更改属性",
+           "author": "AIGODLIKE Community(BlenderCN辣椒,小萌新)",
+           "version": (1, 0),
+           "blender": (3, 0, 0),
+           # "location": "Tool Panel",
+           "support": "COMMUNITY",
+           # "category": "3D View",
+           }
 
 
 def register():
