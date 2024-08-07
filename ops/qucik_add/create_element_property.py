e56f4743d9e4fa208d8003215e0d3bfd7929a71e
小萌新 <3209970865@qq.com>
2024-08-07 16:07:09 +0800

    Feat: create enum

--- ops/qucik_add/create_element_property.py
+++ ops/qucik_add/create_element_property.py
@@ -1,43 +1,104 @@
 import bpy
 from bl_ui.properties_paint_common import UnifiedPaintPanel
 from bpy.app.translations import pgettext
-from bpy.props import EnumProperty, StringProperty, IntProperty, FloatProperty
+from bpy.props import EnumProperty, StringProperty, IntProperty, FloatProperty, BoolProperty
 
 from ...utils.enum import CREATE_ELEMENT_VALUE_MODE_ENUM
 from ...utils.property_data import CREATE_ELEMENT_DATA_PATHS, CREATE_ELEMENT_BRUSH_PATH
 from ...utils.public import get_pref, PublicOperator, PublicProperty
 
 
-class OpsProperty:
+class Enum:
+    enum_mode: EnumProperty(
+        items=[
+            ('SET', '直接设置 枚举值', '使用 bpy.ops.wm.context_set_enum 操作符'),
+            ('CYCLE', '循环设置 枚举值 (如果设置值和当前值相同,则切换到上一个值)',
+             '使用 bpy.ops.wm.context_cycle_enum 操作符'),
+            ('TOGGLE', '切换设置 枚举值 (在两个枚举值之间切换)', '使用 bpy.ops.wm.context_toggle_enum 操作符'),
+            ('MENU', '菜单', '使用 bpy.ops.wm.context_menu_enum 操作符 使用菜单显示枚举'),
+            ('PIE', '饼菜单', '使用 bpy.ops.wm.context_pie_enum 操作符 使用饼菜单显示枚举(最多8个项)'),
+        ],
+        name='枚举模式',
+        options={'HIDDEN', 'SKIP_SAVE'})
+
+    ___enum___ = []  # 防止脏数据
+
+    def __get_enum__(self, context):
+        """获取枚举"""
+        button_prop = getattr(CreateElementProperty, "button_prop",
+                              getattr(context, "button_prop",
+                                      getattr(self, "button_prop", None)
+                                      )
+                              )
+        if button_prop:
+            if button_prop.enum_items:
+                items = button_prop.enum_items
+            elif button_prop.enum_items_static:
+                items = button_prop.enum_items_static
+            elif button_prop.enum_items_static_ui:
+                items = button_prop.enum_items_static_ui
+            else:
+                items = [
+
+                ]
+        else:
+            items = []
+
+        aaa = [(item.identifier, item.name, item.description, item.icon, index)
+               for (index, item) in enumerate(items)]
+        if aaa:
+            if aaa != OpsProperty.___enum___:
+                OpsProperty.___enum___ = aaa
+        else:
+            if OpsProperty.___enum___ != CREATE_ELEMENT_VALUE_MODE_ENUM:
+                OpsProperty.___enum___ = CREATE_ELEMENT_VALUE_MODE_ENUM
+        return OpsProperty.___enum___
+
+    enum_value_a: EnumProperty(options={'HIDDEN', 'SKIP_SAVE'}, items=__get_enum__)
+    enum_value_b: EnumProperty(options={'HIDDEN', 'SKIP_SAVE'}, items=__get_enum__)
+    enum_reverse: BoolProperty(default=False, name="反转", description="循环时反转枚举顺序")
+    enum_wrap: BoolProperty(default=True, name="循环", description="循环时如果是最后一个或第一个值时,会自动跳转")
+
+
+class OpsProperty(Enum):
     boolean_mode: EnumProperty(
         items=[('SET_TRUE', '设置为 True', ''), ('SET_FALSE', '设置为 False', ''), ('SWITCH', '切换', '')],
         name='布尔模式',
-        options={'HIDDEN'})
-    enum_mode: EnumProperty(
-        items=[('CYCLE', '循环设置 枚举值 (如果设置值和当前值相同,则切换到上一个值)',
-                '使用 bpy.ops.wm.context_cycle_enum 操作符'),
-               ('SET', '直接设置 枚举值', '使用 bpy.ops.wm.context_set_enum 操作符')],
-        name='枚举模式',
-        options={'HIDDEN'})
+        options={'HIDDEN', 'SKIP_SAVE'})
+
     data_path: StringProperty(options={'HIDDEN', 'SKIP_SAVE'})
+
     property_type: EnumProperty(items=[
         ("BOOLEAN", "Boolean", ""),
         ("INT", " Integer", ""),
         ("FLOAT", "Float", ""),
         ("STRING", " String", ""),
         ("ENUM", "Enumeration", ""),
-        ("POINTER", " Pointer", ""),
-        ("COLLECTION", "Collection", ""),
+        # ("POINTER", " Pointer", ""),
+        # ("COLLECTION", "Collection", ""),
     ])
 
     value_mode: EnumProperty(items=CREATE_ELEMENT_VALUE_MODE_ENUM, name="值模式")
-    int_value: IntProperty(options={'HIDDEN', 'SKIP_SAVE'}, name="整数值")
-    float_value: FloatProperty(options={'HIDDEN', 'SKIP_SAVE'}, name="浮点值")
+    int_value: IntProperty(options={'HIDDEN', 'SKIP_SAVE'}, name="整数值", default=0)
+    float_value: FloatProperty(options={'HIDDEN', 'SKIP_SAVE'}, name="浮点值", default=0)
+    string_value: StringProperty(options={'HIDDEN', 'SKIP_SAVE'}, name="字符串")
 
     @property
-    def __context_data_path__(self) -> str:
+    def __data_path__(self) -> str:
         """bpy.context.space_data.show_gizmo -> space_data.show_gizmo"""
         return self.data_path.replace("bpy.context.", "")
+
+    @classmethod
+    def clear_info(cls):
+        """清理信息"""
+        cls.button_pointer = None
+        cls.button_prop = None
+
+    @classmethod
+    def from_context_get_info(cls, context) -> None:
+        """从上下文获取信息"""
+        cls.button_pointer = getattr(context, "button_pointer", None)
+        cls.button_prop = getattr(context, "button_prop", None)
 
 
 class Draw(PublicOperator, PublicProperty, OpsProperty):
@@ -47,14 +108,12 @@ class Draw(PublicOperator, PublicProperty, OpsProperty):
         layout = self.layout.column()
         layout.operator_context = "EXEC_DEFAULT"
 
-        pointer = self.button_pointer
-        prop = self.button_prop
-        layout.context_pointer_set('button_pointer', pointer)
-        layout.context_pointer_set('button_prop', prop)
-        layout.context_pointer_set('show_gesture_add_menu', self)
+        self.update_data(layout, context)
 
         pref = get_pref()
 
+        pointer = self.button_pointer
+        prop = self.button_prop
         if prop and ContextMenu.show_context_menu:
             prop_type = prop.type
 
@@ -74,10 +133,6 @@ class Draw(PublicOperator, PublicProperty, OpsProperty):
                     self.draw_string(context, layout)
                 elif prop_type == "ENUM":
                     self.draw_enum(context, layout)
-                elif prop_type == "POINTER":
-                    self.draw_pointer(context, layout)
-                elif prop_type == "COLLECTION":
-                    self.draw_collection(context, layout)
             else:
                 layout.alert = True
                 layout.label(text="无法获取数据路径")
@@ -93,10 +148,12 @@ class Draw(PublicOperator, PublicProperty, OpsProperty):
 
             layout.label(text=f"button_pointer:\t{pointer}")
             layout.label(text=f"button_prop:\t{prop}")
+            layout.label(text=f"a:\t{getattr(context, 'button_prop', None)}")
             layout.label(text=f"subtype:\t{prop.subtype}")
             layout.label(text=f"data_path:\t{self.data_path}")
 
     def draw_boolean(self, context: bpy.types.Context, layout: bpy.types.UILayout):
+        layout.label(text="设置布尔值")
         for item in self.rna_type.properties["boolean_mode"].enum_items:  # 绘制添加的布尔模式
             ops = layout.operator(CreateElementProperty.bl_idname, text=item.name)
             ops.boolean_mode = item.identifier
@@ -127,40 +184,60 @@ class Draw(PublicOperator, PublicProperty, OpsProperty):
         ops.value_mode = self.value_mode
         ops.data_path = self.data_path
         ops.float_value = self.float_value
-        ops.property_type = "INT"
+        ops.property_type = "FLOAT"
 
     def draw_string(self, context: bpy.types.Context, layout: bpy.types.UILayout):
-        ...
+        layout.label(text="修改文本")
+        layout.separator()
+        layout.prop(self, "string_value")
+        layout.separator()
+        ops = layout.operator(CreateElementProperty.bl_idname, text="添加")
+        ops.value_mode = self.value_mode
+        ops.data_path = self.data_path
+        ops.float_value = self.float_value
+        ops.property_type = "STRING"
 
     def draw_enum(self, context: bpy.types.Context, layout: bpy.types.UILayout):
-        button_pointer = self.button_pointer
-        button_prop = self.button_prop
+        layout.label(text="修改枚举值")
 
-        if button_prop.enum_items:
-            items = button_prop.enum_items
-        elif button_prop.enum_items_static:
-            items = button_prop.enum_items_static
-        elif button_prop.enum_items_static_ui:
-            items = button_prop.enum_items_static_ui
-        else:
-            items = []
+        layout.separator()
 
-        if items:
-            layout.prop(self, "enum_mode", text="枚举模式", expand=True)
-            column = layout.column(align=True)
-            for item in items:
-                ops = column.operator(
-                    CreateElementProperty.bl_idname,
-                    text=f'{pgettext(item.name, "*")} ({item.identifier})',
-                    icon=item.icon)
-                ops.enum_mode = self.enum_mode
-                ops.data_path = self.data_path
+        layout.prop(self, "enum_mode", text="枚举模式", expand=True)
+        if self.enum_mode == "TOGGLE":
+            row = layout.row(align=True)
+            a = row.column(align=True)
+            a.label(text="值A")
+            a.prop(self, "enum_value_a", expand=True)
 
-    def draw_pointer(self, context: bpy.types.Context, layout: bpy.types.UILayout):
-        ...
+            b = row.column(align=True)
+            b.label(text="值B")
+            b.prop(self, "enum_value_b", expand=True)
+        elif self.enum_mode == "SET":
+            layout.prop(self, "enum_value_a", expand=True)
 
-    def draw_collection(self, context: bpy.types.Context, layout: bpy.types.UILayout):
-        ...
+        if self.enum_mode == "CYCLE":
+            layout.separator()
+            layout.prop(self, "enum_reverse")
+            layout.prop(self, "enum_wrap")
+
+        layout.separator()
+
+        cc = layout.column(align=True)
+        is_eq = self.enum_mode == "TOGGLE" and self.enum_value_a == self.enum_value_b
+        if is_eq:
+            cc.alert = True
+            cc.label(text="值A 与 值B 相同")
+            cc = cc.column(align=True)
+            cc.enabled = False
+
+        ops = cc.operator(CreateElementProperty.bl_idname, text="添加")
+        ops.data_path = self.data_path
+        ops.enum_mode = self.enum_mode
+        ops.enum_value_a = self.enum_value_a
+        ops.enum_value_b = self.enum_value_b
+        ops.enum_reverse = self.enum_reverse
+        ops.enum_wrap = self.enum_wrap
+        ops.property_type = "ENUM"
 
 
 class Create(Draw):
@@ -170,31 +247,63 @@ class Create(Draw):
         bpy.ops.wm.context_toggle()
         :return:
         """
-        bpy.ops.gesture.element_add(add_active_radio=True, element_type="OPERATOR")
         ae = self.active_element
         if ae:
             bm = self.boolean_mode
             if bm == "SET_TRUE":
-                ae.operator_bl_idname = f"wm.context_set_boolean(data_path='{self.__context_data_path__}', value=True)"
+                ae.operator_bl_idname = f"wm.context_set_boolean(data_path='{self.__data_path__}', value=True)"
             elif bm == "SET_FALSE":
-                ae.operator_bl_idname = f"wm.context_set_boolean(data_path='{self.__context_data_path__}', value=False)"
+                ae.operator_bl_idname = f"wm.context_set_boolean(data_path='{self.__data_path__}', value=False)"
             elif bm == "SWITCH":
-                ae.operator_bl_idname = f"wm.context_toggle(data_path='{self.__context_data_path__}')"
+                ae.operator_bl_idname = f"wm.context_toggle(data_path='{self.__data_path__}')"
 
     def create_int(self):
-        bpy.ops.gesture.element_add(add_active_radio=True, element_type="OPERATOR")
         ae = self.active_element
+        from ..modal_mouse import ModalMouseOperator
         if ae:
             vm = self.value_mode
+            if vm == "SET_VALUE":
+                ae.operator_bl_idname = f"wm.context_set_int(data_path='{self.__data_path__}', value={self.int_value})"
+            else:
+                bl = f"{ModalMouseOperator.bl_idname}(data_path='{self.__data_path__}', value_mode={self.value_mode})"
+                ae.operator_bl_idname = bl
 
     def create_float(self):
-        ...
+        ae = self.active_element
+        from ..modal_mouse import ModalMouseOperator
+        if ae:
+            vm = self.value_mode
+            if vm == "SET_VALUE":
+                ae.operator_bl_idname = f"wm.context_set_float(data_path='{self.__data_path__}', value={self.float_value})"
+            else:
+                bl = f"{ModalMouseOperator.bl_idname}(data_path='{self.__data_path__}', value_mode={self.value_mode})"
+                ae.operator_bl_idname = bl
 
     def create_string(self):
         ...
 
     def create_enum(self):
-        ...
+        """
+        bpy.ops.wm.context_set_enum(data_path="", value="")
+        bpy.ops.wm.context_cycle_enum(data_path="", reverse=False, wrap=False)
+        bpy.ops.wm.context_toggle_enum(data_path="", value_1="", value_2="")
+        bpy.ops.wm.context_menu_enum(data_path="")
+        bpy.ops.wm.context_pie_enum(data_path="")
+        :return:
+        """
+        ae = self.active_element
+        if ae:
+            em = self.enum_mode
+            if em == "SET":
+                ae.operator_bl_idname = f"wm.context_set_enum(data_path='{self.__data_path__}', value='{self.enum_value_a}')"
+            elif em == "CYCLE":
+                ae.operator_bl_idname = f"wm.context_cycle_enum(data_path='{self.__data_path__}', reverse={self.enum_reverse},wrap={self.enum_wrap})"
+            elif em == "TOGGLE":
+                ae.operator_bl_idname = f"wm.context_toggle_enum(data_path='{self.__data_path__}', value_1='{self.enum_value_a}', value_2='{self.enum_value_b}')"
+            elif em == "MENU":
+                ae.operator_bl_idname = f"wm.context_menu_enum(data_path='{self.__data_path__}')"
+            elif em == "PIE":
+                ae.operator_bl_idname = f"wm.context_pie_enum(data_path='{self.__data_path__}')"
 
     def create(self):
         """
@@ -209,6 +318,7 @@ class Create(Draw):
         COLLECTION:Collection.
         :return:
         """
+        bpy.ops.gesture.element_add(add_active_radio=True, element_type="OPERATOR")
         pt = self.property_type
         if pt == "BOOLEAN":
             self.create_boolean()
@@ -226,11 +336,8 @@ class CreateElementProperty(Create):
     bl_label = '创建属性元素'
     bl_idname = 'gesture.create_element_property'
 
-    # bl_options = {'REGISTER', 'UNDO'}
-
-    def __init__(self):
-        self.button_prop = None
-        self.button_pointer = None
+    button_pointer = None
+    button_prop = None
 
     @classmethod
     def poll(cls, context) -> bool:
@@ -239,13 +346,14 @@ class CreateElementProperty(Create):
         return button_pointer and button_prop
 
     def invoke(self, context, event) -> set[str]:
-        print(f"\n{self.bl_idname}\tinvoke")
+        print(f"\n{self.bl_idname}\tinvoke", self)
         self.from_context_get_info(context)
         self.copy_data_path()
 
         return context.window_manager.invoke_popup(**{'operator': self, 'width': 600})
 
     def execute(self, context) -> set[str]:
+        self.clear_info()
         from ...ui.context_menu import ContextMenu
         ContextMenu.show_context_menu = False
 
@@ -253,7 +361,7 @@ class CreateElementProperty(Create):
         name = self.button_pointer.__class__.__name__
         identifier = self.button_prop.identifier
 
-        print("\nexecute", self.data_path)
+        print("\nexecute", self.data_path, name, identifier, )
         self.create()
         return {'FINISHED', "RUNNING_MODAL"}
 
@@ -288,7 +396,22 @@ class CreateElementProperty(Create):
             print("use clipboard", clipboard)
             self.data_path = clipboard
 
-    def from_context_get_info(self, context) -> None:
-        """从上下文获取信息"""
-        self.button_pointer = getattr(context, "button_pointer", None)
-        self.button_prop = getattr(context, "button_prop", None)
+    def update_data(self, layout, context):
+        """更新数据"""
+        pointer = self.button_pointer
+        if pointer:
+            layout.context_pointer_set('button_pointer', pointer)
+        else:
+            po = getattr(context, "button_pointer", None)
+            if po:
+                self.button_pointer = po
+
+        prop = self.button_prop
+        if prop:
+            layout.context_pointer_set('button_prop', prop)
+        else:
+            pr = getattr(context, "button_prop", None)
+            if pr:
+                self.button_prop = pr
+
+        layout.context_pointer_set('show_gesture_add_menu', self)
