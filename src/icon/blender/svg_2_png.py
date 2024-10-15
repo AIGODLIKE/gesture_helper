4bd79e7b0fada5561466aa84730b8eaf545a7f1d
小萌新 <3209970865@qq.com>
2024-10-15 12:28:31 +0800

    Feat: add svg_2_png.py

---
+++ src/icon/blender/svg_2_png.py
@@ -0,0 +1,28 @@
+import os
+
+"""
+1. pip install cairosvg
+2. install gtk
+https://github.com/tschoonj/GTK-for-Windows-Runtime-Environment-Installer/releases
+
+https://www.modb.pro/db/175191
+"""
+
+import cairosvg
+
+size = 128
+to_folder_path = os.path.dirname(__file__)
+form_svg_folder = r"D:\Blender\blender\release\datafiles\icons_svg"
+
+for file_name in os.listdir(form_svg_folder):
+    file_path = os.path.join(form_svg_folder, file_name)
+    if os.path.isfile(file_path) and file_name.endswith(".svg"):
+        new_name = file_name.replace(".svg", ".png").upper().replace(".PNG", ".png")
+        to_path = os.path.join(to_folder_path, new_name)
+        print("SVG to PNG", file_name, "->", new_name)
+        cairosvg.svg2png(
+            url=file_path,
+            write_to=to_path,
+            parent_width=size,
+            parent_height=size
+        )
