2295c0e482c21a93f1e9e811bf8d8906b3a05415
小萌新 <3209970865@qq.com>
2024-10-29 18:31:21 +0800

    Feat: timeout default value 300ms -> 100ms

--- preferences/gesture.py
+++ preferences/gesture.py
@@ -12,7 +12,7 @@ class GestureProperty(PropertyGroup):
         if self.threshold > self.threshold_confirm:
             self['threshold_confirm'] = self.threshold_confirm + 20
 
-    timeout: IntProperty(name='Gesture Timeout(ms)', **gen_gesture_prop(300, 'TIME'))
+    timeout: IntProperty(name='Gesture Timeout(ms)', **gen_gesture_prop(100, 'TIME'))
     radius: IntProperty(name='Gesture Radius', **gen_gesture_prop(120))
     threshold: IntProperty(name='Threshold', **gen_gesture_prop(30))
     threshold_confirm: IntProperty(name='Confirm Threshold', **gen_gesture_prop(80))
